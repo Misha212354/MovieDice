9ebfe642b94c9b370812e29aaed55e0b29f0d73c
Mike <mterekho20@bw.edu>
2023-05-08 15:02:24 -0400

    Make organized Generator class

--- Web Movie Decision/get_data.py
+++ Web Movie Decision/get_data.py
@@ -14,54 +14,58 @@ moviesDB = imdb.IMDb()
 
 # Using the Search movie method
 class Generator:
-    def __init__(self, m_or_s, genre, year, rating):    #, results_num
-        self.m_or_s = m_or_s.lower()
+    def __init__(self, genre, year, rating):    #, results_num
         self.genre = str(genre.lower())
         self.year = int(year)
         self.rating = float(rating)
+        self.list = []
         
-    
-    def gen_out(self):
-        #Movie or TV series###################################################################
-        if(self.m_or_s == "1"):
-            sub_DB = moviesDB.get_top50_movies_by_genres(self.genre)
+    def get_movies(self):
+        #get top 50 movies by genre
+        sub_DB = moviesDB.get_top50_movies_by_genres(self.genre)
 
+        #get additional movies (web scraping)
+        df = pd.read_csv('https://www.imdb.com/list/ls024863935/export', header=0, usecols=["Title", "Title Type", "IMDb Rating", "Year",  "Genres", "Release Date"])
+        df.rename(columns={"Title": "title", "IMDb Rating": "rating", "Year": "year", "Genres":"genre"}, inplace=True)
 
-            df = pd.read_csv('https://www.imdb.com/list/ls024863935/export', header=0, usecols=["Title", "Title Type", "IMDb Rating", "Year",  "Genres", "Release Date"])
-            df.rename(columns={"Title": "title", "IMDb Rating": "rating", "Year": "year", "Genres":"genre"}, inplace=True)
+        for i in range(len(df["genre"])):
+            df["genre"][i] = (df["genre"][i].replace(" ", "")).split(",")
 
-            for i in range(len(df["genre"])):
-                df["genre"][i] = (df["genre"][i].replace(" ", "")).split(",")
-
-            for i in range(len(df["Title Type"])):
-                if df["Title Type"][i] == 'movie':
-                    sub_DB.append(df.loc[i])
-            
-        
-        if(self.m_or_s == "2"):
-            sub_DB = moviesDB.get_top50_tv_by_genres(self.genre)
-
-            
-            df = pd.read_csv("https://www.imdb.com/list/ls000033724/export", header=0, usecols=["Title", "Title Type", "IMDb Rating", "Year",  "Genres", "Release Date"])
-            df.rename(columns={"Title": "title", "IMDb Rating": "rating", "Year": "year", "Genres":"genre"}, inplace=True)
-
-            for i in range(len(df["genre"])):
-                df["genre"][i] = (df["genre"][i].replace(" ", "")).split(",")
-
-            for i in range(len(df["Title Type"])):
+        for i in range(len(df["Title Type"])):
+            if df["Title Type"][i] == 'movie':
                 sub_DB.append(df.loc[i])
-        ######################################################################################
 
-        #Parse through years and rating#######################################################
-        g =[]
-        for n in sub_DB:
+        self.list = sub_DB
+
+    def get_tv_shows(self):
+        #get top 50 tv shows by genre
+        sub_DB = moviesDB.get_top50_movies_by_genres(self.genre)
+
+        df = pd.read_csv('https://www.imdb.com/list/ls024863935/export', header=0, usecols=["Title", "Title Type", "IMDb Rating", "Year",  "Genres", "Release Date"])
+        df.rename(columns={"Title": "title", "IMDb Rating": "rating", "Year": "year", "Genres":"genre"}, inplace=True)
+
+        for i in range(len(df["genre"])):
+            df["genre"][i] = (df["genre"][i].replace(" ", "")).split(",")
+
+        for i in range(len(df["Title Type"])):
+            if df["Title Type"][i] == 'movie':
+                sub_DB.append(df.loc[i])
+
+        self.list = sub_DB
+
+    def year_rating_clean(self):
+        new_list = []
+
+        for n in self.list:
             if n.get("year") != None and n.get("rating") != None and len(n["genre"]) != 0:
                 if n["year"] >= self.year and n["year"] <= date.today().year and n["rating"] >= self.rating and self.genre in np.char.lower(n["genre"]):
-                    g.append(n)
-        sub_DB = g
-        ######################################################################################
-        if len(sub_DB) != 0:
-            rand = random.choice(sub_DB)
+                    new_list.append(n)
+
+        self.list = new_list
+
+    def make_choice(self):
+        if len(self.list) != 0:
+            rand = random.choice(self.list)
             
             chosen_movie = moviesDB.search_movie_advanced(str(rand['title']))[0]
             url = chosen_movie["cover url"]
--- Web Movie Decision/playground/views.py
+++ Web Movie Decision/playground/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 from .forms import Info
-import get_data as g
+from get_data import Generator as Generator
 import socket
 socket.setdefaulttimeout(150 * 6000)
 
@@ -15,16 +15,28 @@ def crit(response):
             genre = form.cleaned_data["genre"]
             year = form.cleaned_data["year"]
             rating = form.cleaned_data["rating"]
-            
-            data = g.Generator(movie_series, genre, year, rating).gen_out()
 
-            title = data[0]
-            year = data[1]
-            genre = data[2]
-            rating = data[3]
-            plot = data[4]
-            cover = data[5]
-            r = data[6]
+            generator = Generator(genre, year, rating)
+
+            if (movie_series == "1"): 
+                generator.get_movies()
+
+            if (movie_series == "2"):
+                generator.get_tv_shows()
+
+            generator.year_rating_clean()
+            chosen = generator.make_choice()
+
+            
+            #data = g.Generator(movie_series, genre, year, rating).
+
+            title = chosen[0]
+            year = chosen[1]
+            genre = chosen[2]
+            rating = chosen[3]
+            plot = chosen[4]
+            cover = chosen[5]
+            r = chosen[6]
             return render(response, "WMD.html", {"form":form,
                                                 "title":title, 
                                                 "year":year, 
